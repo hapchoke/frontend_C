eafce6c1296e3bf2fe52224e0477063c79391570
hapchoke <80566116+hapchoke@users.noreply.github.com>
2022-10-31 22:21:18 +0900

    create store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { arrayMoveSlice } from '../features/Array/arraySlice';
 
+// ルートreducer作成
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: arrayMoveSlice.reducer,
 });
